888ab72447f606204f32994ecedfbc0f9dceb9bd
Julian Wolf <julian.wolf@invenium.io>
2022-11-12 22:43:24 +0100

    feat: Async loading in FE

--- frontend/src/components/ListingTable.svelte
+++ frontend/src/components/ListingTable.svelte
@@ -4,12 +4,34 @@
     import {attributes} from "../stores/Attributes";
     import {onMount} from "svelte";
     import TD from "./table/TD.svelte";
+    import {logDebug} from "../modules/Extensions";
 
     let _keys = []
     attributes.subscribe(as => _keys = as)
 
     let _listings = []
-    listings.subscribe(ls => _listings = ls)
+    let visibleListings = []
+    let visibleTimer = null
+
+    listings.subscribe(ls => {
+        _listings = ls
+        if (visibleTimer)
+            clearInterval(visibleTimer)
+
+        let steps = 20
+        visibleListings = _listings.slice(0, steps)
+
+        let vlidx = steps
+        visibleTimer = setInterval(() => {
+            if (vlidx >= _listings.length)
+                return clearTimeout(visibleTimer)
+
+            visibleListings = [...visibleListings, ..._listings.slice(vlidx, vlidx + steps)]
+            vlidx += steps
+
+            logDebug(vlidx)
+        }, 10)
+    })
 
     onMount(() => {
         attributes.refresh()
@@ -32,20 +54,18 @@
             </tr>
             </thead>
             <tbody>
-            {#each _listings as listing, idx}
-                {#if idx < 10}
-                    <tr>
-                        <td>
-                            <a target="_blank"
-                               href="https://www.willhaben.at/iad/object?adId={listing.id}">
-                                {listing.id}
-                            </a>
-                        </td>
-                        {#each _keys as key}
-                            <TD content={listing.attributes[key.id] ? listing.attributes[key.id].join(" ") : "" }></TD>
-                        {/each}
-                    </tr>
-                {/if}
+            {#each visibleListings as listing}
+                <tr>
+                    <td>
+                        <a target="_blank"
+                           href="https://www.willhaben.at/iad/object?adId={listing.id}">
+                            {listing.id}
+                        </a>
+                    </td>
+                    {#each _keys as key}
+                        <TD content={listing.attributes[key.id] ? listing.attributes[key.id].join(" ") : "" }></TD>
+                    {/each}
+                </tr>
             {/each}
             </tbody>
         </table>
